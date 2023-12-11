00b2734069b2e2bf5b520fd81e878990a7647762
Your Name <you@example.com>
2023-12-10 22:46:20 -0600

    Updated code to add the functionality that tracks the total scores of both the humanand the computer.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
-
+/**
+ * This holds the total scores of both the player and the computer.
+ */
  const totalScores = {computer: 0, player: 0}
 
 /**
@@ -18,7 +20,7 @@ function getComputerChoice(){
     * @returns - returns the result of -1, 0, or 1 based on who won or if it is a tie
     * 
     * This function compares the picks of Rock, Paper, or Scissors
-    *  and returns the score based on these picks.
+    *  and returns the score based on these picks. Also updates the total scores.
  */
 
 function getResult(playerChoice, computerChoice) {
@@ -27,22 +29,29 @@ function getResult(playerChoice, computerChoice) {
     score = 0
   }else if(playerChoice === 'Scissors' && computerChoice === 'Rock'){
     score = -1
+    totalScores['computer']++
   }else if(playerChoice === 'Paper' && computerChoice === 'Scissors'){
     score = -1
+    totalScores['computer']++
   }else if(playerChoice === 'Rock' && computerChoice === 'Paper'){
     score = -1
+    totalScores['computer']++
   }else{
     score = 1
+    totalScores['player']++
   }
   return score
 }
 
-//showResult updates the DOM to `You Win!` or `You Lose!` or `It's a Draw!` based on the score. Also shows Player Choice vs. Computer Choice
 /**
  * 
- * @param {*} score 
- * @param {*} playerChoice 
- * @param {*} computerChoice 
+ * @param score - instant score
+ * @param  playerChoice - It could one of Rock, Paper, or Scissors
+ * @param  computerChoice - It could also be one of Rock, Paper, or Scissors
+ * 
+ * This function takes instant score, player's choice, and the computer's choice and
+ * shows if it is a draw, win, or loss based on the score. If score = -1, it is a loss.
+ * If it is 1, it is a win, and if 0 it is a tie.
  */
 function showResult(score, playerChoice, computerChoice) {
 
@@ -56,35 +65,39 @@ function showResult(score, playerChoice, computerChoice) {
   }else{
     resultDiv.innerText = 'You Won!'
   }
-  playerScoreDiv.innerText = `${Number(playerScoreDiv.innerText) + score}`
+  playerScoreDiv.innerText = `Human Score: ${totalScores['player']} Computer Score: ${totalScores['computer']}`
   handsDiv.innerText = `Human: ${playerChoice} vs Computer: ${computerChoice}`
 }
 
-// Calculate who won and show it on the screen
+/**
+ * 
+ * @param playerChoice 
+ * This function displays the result based on which button is clicked.
+ */
 function onClickRPS(playerChoice) {
-  console.log({playerChoice})
   const computerChoice = getComputerChoice()
-  console.log({computerChoice})
   const score = getResult(playerChoice.value, computerChoice)
-  totalScores['player'] += score
   showResult(score,playerChoice.value, computerChoice)
 }
 
-
-// Make the buttons actively listen for a click and do something once a click is detected
+/**
+ * This function listens for the click from the user and performed associated tasks once
+ * the click is detected.
+ */
 function playGame() {
-  // use querySelector to select all RPS Buttons
+  //This selects all of the three buttons
     const rpsBtns = document.querySelectorAll('.rpsButton')
     rpsBtns.forEach(btn =>{
         btn.onclick = () => onClickRPS(btn)
     })
- 
-  //Add a click listener to the end game button that runs the endGame() function on click
+
   let endGameButton = document.getElementById('endGameButton')
   endGameButton.onclick = () => endGame()
 }
 
-//endGame function clears all the text on the Screen
+/**
+ * This function clears all the text and scores from the screen
+ */
 function endGame() {
     let resultDiv = document.getElementById('result')
     let handsDiv = document.getElementById('hands')
